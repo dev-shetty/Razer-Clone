3a7bc3ed416265ef5f8e62260cddde26911ff3f0
Deveesh Shetty <deveeshshetty@gmail.com>
2023-01-22 17:18:44 +0530

    Added notification below nav and razer logo to title

--- frontend/src/components/UiComponents/Navbar/Navbar.tsx
+++ frontend/src/components/UiComponents/Navbar/Navbar.tsx
@@ -1,27 +1,31 @@
 import { AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai"
+import Notification from "../Notification/Notification"
 function Navbar() {
   return (
-    <nav className="flex items-center justify-center gap-16 py-3 border-b-[1px] border-b-razer-green">
-      <ul className="ml-auto">
-        <img src="Misc/razer-logo.svg" alt="Razer Logo" className="w-8" />
-      </ul>
-      <ul className="flex items-center gap-16">
-        <li>PC</li>
-        <li>Console</li>
-        <li>Mobile</li>
-        <li>Lifestyle</li>
-        <li>Services</li>
-        <li>Commuinity</li>
-        <li>Support</li>
-        <li>Store</li>
-        <li className="text-primary-400">
-          <AiOutlineSearch className="scale-150" />
-        </li>
-      </ul>
-      <ul className="mr-auto text-primary-400">
-        <AiOutlineShoppingCart className="scale-150" />
-      </ul>
-    </nav>
+    <>
+      <nav className="flex items-center justify-center gap-16 py-3 border-b-[1px] border-b-razer-green">
+        <ul className="ml-auto">
+          <img src="Misc/razer-logo.svg" alt="Razer Logo" className="w-8" />
+        </ul>
+        <ul className="flex items-center gap-16">
+          <li>PC</li>
+          <li>Console</li>
+          <li>Mobile</li>
+          <li>Lifestyle</li>
+          <li>Services</li>
+          <li>Commuinity</li>
+          <li>Support</li>
+          <li>Store</li>
+          <li className="text-primary-400">
+            <AiOutlineSearch className="scale-150" />
+          </li>
+        </ul>
+        <ul className="mr-auto text-primary-400">
+          <AiOutlineShoppingCart className="scale-150" />
+        </ul>
+      </nav>
+      <Notification />
+    </>
   )
 }
 
---
+++ frontend/src/components/UiComponents/Notification/Notification.tsx
@@ -0,0 +1,12 @@
+function Notification() {
+  return (
+    <div className="flex justify-center px-2 py-2 text-xs text-center bg-primary-200 text-primary-100">
+      <p>
+        New Year's Specials: Ready up for 2023 with our best picks and latest
+        CES releases. Shop Now {`>`}
+      </p>
+    </div>
+  )
+}
+
+export default Notification
